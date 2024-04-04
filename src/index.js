e93ca5cf1acfcb8d07730e9ba6769c15e6dfc443
krishnakichu <krishnaprasath2202@gmail.com>
2024-04-04 20:36:40 +0530

    my first react learnings

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+// 1) Import the React and ReactDOM Lib
+import React  from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//2) Get a reference to the div with ID Root
+const el = document.getElementById("root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+//3) Tell React to Take control of that element
+const root = ReactDOM.createRoot(el);
+
+//4) Create a Component
+
+//export default function App(){
+    // const date = new Date();
+    // const time = date.toLocaleTimeString();
+    // const name = 'Sam';
+    // const age = 21;
+    // return(
+    //     <h1>
+    //         Hi I am {name}, I am {age}
+    //     </h1>
+    //     );
+    // 
+    //return <input maxLength={5} />
+//};
+//5) Show the COmponent the screen
+root.render(<App />);
